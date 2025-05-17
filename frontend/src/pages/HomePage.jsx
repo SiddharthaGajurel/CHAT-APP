44bd3e2d3ae099c8ff599d0784012b8b559bef34
Siddhartha Gajurel <gajurelsiddhartha305@gmail.com>
2025-05-18 00:19:47 +1000

    HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   return (
     <div className="min-h-screen bg-base-200">
       <div className="flex flex-col md:flex-row items-center justify-center pt-8 md:pt-20 px-2 md:px-4">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl min-h-[calc(100vh-8rem)] md:h-[calc(100vh-8rem)] flex flex-col md:flex-row overflow-hidden">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl min-h-screen flex flex-col md:flex-row overflow-auto pb-8">
           <Sidebar />
 
           {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
