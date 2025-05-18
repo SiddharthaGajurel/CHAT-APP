3e7e5f3e298e253ab41c05c308fa1fa0d200eb53
Siddhartha Gajurel <gajurelsiddhartha305@gmail.com>
2025-05-18 13:14:31 +1000

    SettingsPage.jsx

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -57,7 +57,7 @@ const SettingsPage = () => {
                       J
                     </div>
                     <div>
-                      <h3 className="font-medium text-sm">Siddhartha Gajurel</h3>
+                      <h3 className="font-medium text-sm">Just a user</h3>
                       <p className="text-xs text-base-content/70">Online</p>
                     </div>
                   </div>
